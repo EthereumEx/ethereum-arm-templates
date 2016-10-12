ce3045485f43a2fb66cffe7162c6f63497c49ff8
cicorias <github@cicoria.com>
2016-10-12 15:43:02 +1100

    updates

--- ethereum-consortium-add-member/scripts/start-private-blockchain.sh
+++ ethereum-consortium-add-member/scripts/start-private-blockchain.sh
@@ -29,7 +29,7 @@ fi
 
 VERBOSITY=4;
 
-nohup geth --datadir $GETH_HOME -verbosity $VERBOSITY --bootnodes $BOOTNODE_URLS --maxpeers $MAX_PEERS --nat none --networkid $NETWORK_ID --identity $IDENTITY $MINE_OPTIONS $FAST_SYNC --rpc --rpcaddr "$IPADDR" --rpccorsdomain "*" >> $GETH_LOG_FILE_PATH 2>&1 &
+nohup geth --datadir $GETH_HOME --bootnodes $BOOTNODE_URLS --maxpeers $MAX_PEERS --nat none --networkid $NETWORK_ID --identity $IDENTITY $MINE_OPTIONS $FAST_SYNC --rpc --rpcaddr "$IPADDR" --rpccorsdomain "*" >> $GETH_LOG_FILE_PATH 2>&1 &
 echo "===== Started geth =====";
 
 ################
