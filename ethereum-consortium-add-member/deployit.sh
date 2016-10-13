52e9ead27fb29815d63c35388aa304cc59ee6433
cicorias <github@cicoria.com>
2016-10-13 15:32:34 +1100

    update genesis

--- ethereum-consortium-add-member/deployit.sh
+++ ethereum-consortium-add-member/deployit.sh
@@ -3,7 +3,7 @@
 # TODO:  collision detection on groups
 
 GROUP=$1;
-LOCATION="australiaeast";
+LOCATION="australiasoutheast";
 
 azure group create $GROUP --location $LOCATION -t blockchain=yes;
 azure group deployment create -v -g $GROUP -n arm1 -f ./azuredeploy.json -e ./deploy-test.json 
