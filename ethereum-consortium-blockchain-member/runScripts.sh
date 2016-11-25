9010d974f3dae270daaba6692128cbbba69952f7
Eric Maino <ericmai@microsoft.com>
2016-11-24 22:06:13 -0800

    Adding additional logging to runScripts.sh

--- ethereum-consortium-blockchain-member/runScripts.sh
+++ ethereum-consortium-blockchain-member/runScripts.sh
@@ -10,8 +10,10 @@ SCRIPT_COMPLETE=$SCRIPTS/.complete.0
 
 if [ ! -f $SCRIPT_COMPLETE ]; then
   export DEBIAN_FRONTEND=noninteractive
-  apt-mark hold walinuxagent
-  $CURL -o $SCRIPTS/installDocker.sh https://gist.githubusercontent.com/ericmaino/32d7155dedcf3d020f3a35bcea494ff7/raw/6824ea7d00f4c939c0f0ecba3351060a2820d2ea/install-docker-ubuntu1604.sh >> $LOG 2>&1
+  apt-mark hold walinuxagent >> $LOG 2>&1
+  URL=https://gist.githubusercontent.com/ericmaino/32d7155dedcf3d020f3a35bcea494ff7/raw/6824ea7d00f4c939c0f0ecba3351060a2820d2ea/install-docker-ubuntu1604.sh
+  echo "Downloading from $URL" >> $LOG 2>&1
+  $CURL -o $SCRIPTS/installDocker.sh $URL >> LOG 2>&1
   sh $SCRIPTS/installDocker.sh >> $LOG 2>&1
   echo > $SCRIPT_COMPLETE
 fi
