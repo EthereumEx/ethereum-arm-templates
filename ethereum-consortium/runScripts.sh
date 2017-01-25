0a4cf7254fc6b7413283208e080925ccac296142
Eric Maino <ericmai@microsoft.com>
2017-01-26 09:31:27 +1100

    Refactoring with a download function

--- ethereum-consortium/runScripts.sh
+++ ethereum-consortium/runScripts.sh
@@ -4,6 +4,18 @@ LOG=/root/install.log
 SCRIPTS=/root/scripts
 CURLARGS="-S -s --connect-timeout 5 --retry 15"
 
+download() {
+  echo >> $LOG 2>&1
+  date >> $LOG 2>&1
+  echo "Downloading from $1" >> $LOG 2>&1
+  curl $CURLARGS -o $2 $1/initNode.sh >> $LOG 2>&1
+
+  if [ ! -f $2 ]; then
+    echo "Failed to download $2" >> $LOG 2>&1
+    exit 2
+  fi
+}
+
 date > $LOG
 echo $0 $@ >> $LOG
 mkdir -p $SCRIPTS
@@ -13,25 +25,13 @@ if [ ! -f $SCRIPT_COMPLETE ]; then
   export DEBIAN_FRONTEND=noninteractive
   apt-mark hold walinuxagent >> $LOG 2>&1
   URL=https://gist.githubusercontent.com/ericmaino/32d7155dedcf3d020f3a35bcea494ff7/raw/6824ea7d00f4c939c0f0ecba3351060a2820d2ea/install-docker-ubuntu1604.sh
-  echo "Downloading from $URL" >> $LOG 2>&1
-  curl $CURLARGS -o $SCRIPTS/installDocker.sh $URL >> $LOG 2>&1
+  download $URL $SCRIPTS/installDocker.sh
   sh $SCRIPTS/installDocker.sh >> $LOG 2>&1
   echo > $SCRIPT_COMPLETE
 fi
 
-echo >> $LOG 2>&1
-date >> $LOG 2>&1
-echo "Downloading from $1" >> $LOG 2>&1
 SCRIPT=$SCRIPTS/setupMachine.sh
-curl $CURLARGS -o $SCRIPT $1/initNode.sh >> $LOG 2>&1
-
-if [ ! -f $SCRIPT ]; then
-  echo "Failed to download $SCRIPT" >> $LOG 2>&1
-  exit 2
-fi
-
-NEWARGS=
-SKIP=
+download $1/initNode.sh $SCRIPT 
 
 for ARG in "$@"
 do
